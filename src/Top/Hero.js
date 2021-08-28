276718bb09e952cb080ab22f2e0eb6018596d944
mayur-samrutwar <samrutwarmayur1@gmail.com>
2021-08-28 23:15:33 +0530

    Update

--- src/Top/Hero.js
+++ src/Top/Hero.js
@@ -3,7 +3,7 @@ import Assign from "../components/Assignments/Assign"
 
 export default function Hero (props){
   return (
-    <div className="mb-16">
+    <div className="mb-16" id="hero">
       <div className="bg-gray-50">
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
--- src/Top/Nav.js
+++ src/Top/Nav.js
@@ -9,18 +9,19 @@ export default function Nav({currSem, setCurrSem}) {
     <div class="z-0 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-0 lg:px-0 sticky top-0 z-40 backdrop-filter backdrop-blur-sm bg-white bg-opacity-40">
       <div class="relative flex items-center justify-between">
         <Link
-          to="/"
+          to="hero"
           spy={true} smooth={true}
           aria-label="ECE"
           title="ECE"
           class="cursor-pointer inline-flex items-center"
-        >
+        ><div className="flex">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
           </svg>
           <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
             ECE
           </span>
+          </div>
         </Link>
         <ul class="flex items-center hidden space-x-8 lg:flex">
           <li>
@@ -107,7 +108,7 @@ export default function Nav({currSem, setCurrSem}) {
                 <div class="flex items-center justify-between mb-4">
                   <div>
                     <Link
-                      to="/"
+                      to="hero"
                       spy={true} smooth={true}
                       aria-label="ECE"
                       title="ECE"
